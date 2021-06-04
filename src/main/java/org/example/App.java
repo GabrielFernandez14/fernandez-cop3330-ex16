96302510a1039e19f8c8fb51740d56eae0f50e45
GabrielFernandez14 <79731548+GabrielFernandez14@users.noreply.github.com>
2021-06-04 13:17:42 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
-/* Gabriel Fernandez
- * COP3330 - Summer CV01
- * "Motivated" Practice Exercises
- * Exercise 16 - Legal Driving Age
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Gabriel Fernandez
  */
 
 package org.example;
